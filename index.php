629b3970cfed0eb13041d55205fbde424692328e
EroAuditore <alfa.fenrir@gmail.com>
2020-03-06 11:53:38 -0600

    Intento de subir archivos

--- index.php
+++ index.php
@@ -17,17 +17,44 @@ $app = new Slim\App([
 // This is the middleware
 // It will add the Access-Control-Allow-Methods header to every request
 
+/*
 $app->add(function ($req, $res, $next) {
     $response = $next($req, $res);
     return $response
             ->withHeader('Access-Control-Allow-Origin', '*')
-            ->withHeader('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Accept, Origin, Authorization')
+            ->withHeader('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Accept, Origin, Authorization, x-requested-with, X-CSRF-TOKEN')
             ->withHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+});*/
+
+$app->add(function($request, $response, $next) {
+    $route = $request->getAttribute("route");
+
+    $methods = [];
+
+    if (!empty($route)) {
+        $pattern = $route->getPattern();
+
+        foreach ($this->router->getRoutes() as $route) {
+            if ($pattern === $route->getPattern()) {
+                $methods = array_merge_recursive($methods, $route->getMethods());
+            }
+        }
+        //Methods holds all of the HTTP Verbs that a particular route handles.
+    } else {
+        $methods[] = $request->getMethod();
+    }
+
+    $response = $next($request, $response);
+
+
+    return $response->withHeader("Access-Control-Allow-Methods", implode(",", $methods))
+                    ->withHeader('Access-Control-Allow-Origin', '*')
+    ;
 });
 
-
-require_once "src/routes/routes.php";
 require_once "src/config/db.php";
+require_once "src/routes/routes.php";
+
 
 
 $app->run();
--- src/controllers/depositos.php
+++ src/controllers/depositos.php
@@ -3,7 +3,17 @@
 use \Psr\Http\Message\ServerRequestInterface as Request;
 use \Psr\Http\Message\ResponseInterface as Response;
 
+if (!function_exists('array_key_first')) {
+    function array_key_first(array $arr) {
+        foreach($arr as $key => $unused) {
+            return $key;
+        }
+        return NULL;
+    }
+}
 
+$container = $app->getContainer();
+$container['upload_directory'] = __DIR__ . '/uploads';
 
 //Get todos los tickets;
 $app->get('/api/depositos', function(Request $request, Response $response){
@@ -31,3 +41,63 @@ $app->get('/api/depositos', function(Request $request, Response $response){
     };
 
 });
+
+
+
+//Get todos los tickets;
+$app->post('/api/depositos/valida', function(Request $request, Response $response){
+    $directory = $this->get('upload_directory');
+    $uploadedFiles = $request->getUploadedFiles();
+    /*$id = $request->getAttribute('id');*/
+    $id = 3;
+    $statusValid = 1;    
+
+    $sql = "update depositos set validado=:validado and evidencia=:evidencia where _id=:_id";
+
+    try{
+
+
+        $uploadedFiles = $request->getUploadedFiles();
+        // handle single input with single file upload
+        $uploadedFile = file_get_contents($uploadedFiles["IMG_0879_jpg"]);
+        
+        $db = new db();
+        $db = $db->connectDB();
+        $stmt = $db->prepare($sql);
+        $stmt->bindParam(":evidencia", $uploadedFile, PDO::PARAM_LOB);
+        $stmt->bindParam(":validado", $statusValid);
+        $stmt->bindParam(":_id", $id);
+        
+        $stmt->execute();
+        /*echo json_encode("validado.");*/
+
+        $db = null;
+
+    }catch (PDOException $e)
+    {
+        echo '{"error": { "text":'.$e->getMessage().'}';
+
+    };
+
+});
+
+
+
+/**
+ * Moves the uploaded file to the upload directory and assigns it a unique name
+ * to avoid overwriting an existing uploaded file.
+ *
+ * @param string $directory directory to which the file is moved
+ * @param UploadedFile $uploadedFile file uploaded file to move
+ * @return string filename of moved file
+ */
+function moveUploadedFile($directory, UploadedFile $uploadedFile)
+{
+    $extension = pathinfo($uploadedFile->getClientFilename(), PATHINFO_EXTENSION);
+    $basename = bin2hex(random_bytes(8)); // see http://php.net/manual/en/function.random-bytes.php
+    $filename = sprintf('%s.%0.8s', $basename, $extension);
+
+    $uploadedFile->moveTo($directory . DIRECTORY_SEPARATOR . $filename);
+
+    return $filename;
+}
