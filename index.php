5e3e26c6bf9a52b24b4d8657a95a6e5c1643c3e2
EroAuditore <alfa.fenrir@gmail.com>
2021-03-09 18:48:30 -0600

    (Delete) Comments

--- index.php
+++ index.php
@@ -28,33 +28,6 @@ $app->add(function ($req, $res, $next) {
             ->withHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
 });
 
-/*
-$app->add(function($request, $response, $next) {
-    $route = $request->getAttribute("route");
-
-    $methods = [];
-
-    if (!empty($route)) {
-        $pattern = $route->getPattern();
-
-        foreach ($this->router->getRoutes() as $route) {
-            if ($pattern === $route->getPattern()) {
-                $methods = array_merge_recursive($methods, $route->getMethods());
-            }
-        }
-        //Methods holds all of the HTTP Verbs that a particular route handles.
-    } else {
-        $methods[] = $request->getMethod();
-    }
-
-    $response = $next($request, $response);
-
-
-    return $response->withHeader("Access-Control-Allow-Methods", implode(",", $methods))
-                    ->withHeader('Access-Control-Allow-Origin', '*')
-    ;
-});
-*/
 require_once "src/config/db.php";
 require_once "src/routes/routes.php";
 
